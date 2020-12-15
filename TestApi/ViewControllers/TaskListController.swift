eadab91d86f1b6da188a53e6156bf2420eb75525
YaroslavArk <fast.yarik@gmail.com>
2020-12-15 23:51:03 +0200

    Improved taskList (adde title, tableviewDelegates )

--- TestApi/ViewControllers/TaskListController.swift
+++ TestApi/ViewControllers/TaskListController.swift
@@ -15,7 +15,9 @@ class TaskListController: UIViewController  {
     let tableView = UITableView(frame: .zero, style: .plain)
     let refreshControl = UIRefreshControl()
 
+    var networkManager = NetworkManager.shared
     
+    var dataSource: [Task] = Array.init(repeating: Task(id: 2, title: "Title", dueBy: 1231312, priority: .high, taskDescription: "HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello"), count: 200)
     override func loadView() {
         super.loadView()
         self.view.backgroundColor = .white
@@ -24,9 +26,21 @@ class TaskListController: UIViewController  {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.addSubview(refreshControl)
+        
+        self.title = "Task list"
+        tableView.allowsSelection = true
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        let request = RequestBuilder.getTasksRequest(page: 0, sortedBy: "title", sortingType: .asc)
+        networkManager.makeRequest(request) { (response, object) in
+            print(try! JSONSerialization.jsonObject(with: object as! Data, options: []))
+            
+            
+        } failure: { (error) in
+            assertionFailure(error.localizedDescription)
+        }
+
         self.view.setNeedsUpdateConstraints()
     }
     
@@ -49,19 +63,30 @@ extension TaskListController:UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         70
     }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let cell = cell as? TaskListTableViewCell else { return }
+        cell.task = dataSource[indexPath.row]
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = TaskDetailViewController(with: dataSource[indexPath.row])
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
 extension TaskListController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return dataSource.count
     }
     
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as? TaskListTableViewCell else {
             assertionFailure("Cant cast cell")
             return UITableViewCell()
         }
+        cell.selectionStyle = .none
         return cell
     }
     
