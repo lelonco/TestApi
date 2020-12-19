549a864b66b187438d90c12761f71e8a3bea7b0a
YaroslavArk <fast.yarik@gmail.com>
2020-12-19 03:24:01 +0200

    updated Task model

--- TestApi/Models/Task.swift
+++ TestApi/Models/Task.swift
@@ -10,24 +10,26 @@ import RealmSwift
 
 enum TaskPriority:String, Codable, CaseIterable {
     case low = "Low"
-    case medium = "Medium"
+    case normal = "Normal"
     case high = "High"
 }
 @objcMembers
 class Task: Object, Codable {
     
     enum CodingKeys:String, CodingKey {
-        case id
+        case task
+        case id 
         case title
         case dueBy
         case taskDescription
         case priority
     }
-    
+    dynamic var taskUUID = UUID().uuidString
     dynamic var id = RealmOptional<Int>()
     dynamic var title: String? = nil
     dynamic var dueBy = RealmOptional<Int64>()
     dynamic var priorityString: String?
+    dynamic var wasEdited: Bool = false
     var priority: TaskPriority? {
         get {
             guard let string = priorityString else { return nil }
@@ -43,25 +45,33 @@ class Task: Object, Codable {
         self.title = title
         self.dueBy = RealmOptional(dueBy)
         self.priorityString = priority?.rawValue
-//        self.priority = priority
         self.taskDescription = taskDescription
     }
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-//        try container.encode(id.value, forKey: .id)
         try container.encode(title, forKey: .title)
         try container.encode(dueBy.value, forKey: .dueBy)
-//        try container.encode(taskDescription, forKey: .taskDescription)
         try container.encode(priorityString, forKey: .priority)
     }
     
     required convenience init(from decoder: Decoder) throws {
         self.init()
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.id = try RealmOptional<Int>(container.decode(Int.self, forKey: .id))
-        self.title = try container.decode(String.self, forKey: .title)
-        self.dueBy = try RealmOptional<Int64>( container.decode(Int64.self, forKey: .dueBy))
-        self.priorityString = try container.decode(String.self, forKey: .priority)
-        self.taskDescription = try container.decode(String.self, forKey: .taskDescription)
+        if let task = try? container.nestedContainer(keyedBy: CodingKeys.self, forKey: .task) {
+            self.id = try RealmOptional<Int>(task.decode(Int.self, forKey: .id))
+            self.title = try task.decode(String.self, forKey: .title)
+            self.dueBy = try RealmOptional<Int64>( task.decode(Int64.self, forKey: .dueBy))
+            self.priorityString = try task.decode(String.self, forKey: .priority)
+            self.taskDescription = try? task.decode(String.self, forKey: .taskDescription)
+        } else {
+            self.id = try RealmOptional<Int>(container.decode(Int.self, forKey: .id))
+            self.title = try container.decode(String.self, forKey: .title)
+            self.dueBy = try RealmOptional<Int64>( container.decode(Int64.self, forKey: .dueBy))
+            self.priorityString = try container.decode(String.self, forKey: .priority)
+            self.taskDescription = try? container.decode(String.self, forKey: .taskDescription)
+        }
+    }
+    override class func primaryKey() -> String? {
+        return "taskUUID"
     }
 }
